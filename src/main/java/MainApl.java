7702cca9830ce975ab2a82bea1c2c7691de41ad0
JaToT <totopots@mail.ru>
2025-05-19 08:09:36 +0300

    изменения

--- src/main/java/MainApl.java
+++ src/main/java/MainApl.java
@@ -3,9 +3,11 @@ public class MainApl {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("1111");
+        System.out.println("pip");
     }
 
-    public static void pri() {
+    public static void something() {
         System.out.println("gg");
+        System.out.println("---");
     }
 }
