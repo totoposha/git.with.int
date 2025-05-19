816bb9067ca0bb4eb3df2b8cd4ae0ac77afbb296
JaToT <totopots@mail.ru>
2025-05-19 08:13:02 +0300

    изменения 2

--- src/main/java/MainApl.java
+++ src/main/java/MainApl.java
@@ -1,11 +1,11 @@
 
 public class MainApl {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("bue");
         System.out.println("1111");
     }
 
     public static void pri() {
-        System.out.println("gg");
+        System.out.println("bb");
     }
 }
